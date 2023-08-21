19655a48a4ea428109fd6e97cbd8c3793164ca3d
Yixsi <yixsi3103@gmail.com>
2023-08-20 22:26:30 -0500

    feat(Root): rendered prompt cards from data fetched

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,50 @@
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
+
+export default function CreatePrompt() {
+
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    });
+
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        console.log('called');
+        setSubmitting(true);
+        try {
+            const res = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            });
+            
+            if (res.ok) router.push('/');
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setSubmitting(false);
+        }
+    }
+
+  return (
+    <Form
+        type='Create'
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+    />
+  )
+}
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,58 @@
+'use client'
+import PromptCard from './PromptCard'
+import { useState, useEffect } from 'react'
+
+const PropmCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {
+        data.map((post, index) => (
+          <PromptCard
+            key={index}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))
+      }
+    </div>
+  )
+}
 
 export default function Feed() {
-  return (
-    <div>
+const [searchText, setSearchText] = useState('');
+const [posts, setPosts] = useState([]);
+
+const handleSearch = (e) => {
+  e.preventDefault();
+  setSearchText(e.target.value);
+}
+
+const fetchData = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
       
-    </div>
+      setPosts(data);
+    }
+    
+  useEffect(() => {
+    fetchData();
+    }, []);
+
+  return (
+    <section className='feed'>
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username..."
+          value={searchText}
+          onChange={handleSearch}
+          className="search_input peer"
+        />
+      </form>
+      <PropmCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
